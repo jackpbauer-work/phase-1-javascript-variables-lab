396784d750f326c10d0e870c7389b12694bbe0f2
Jack Bauer <jackbauer.work@gmail.com>
2023-02-21 21:06:36 -0500

    Javascript Variables

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution in this file!
+const companyName = 'Scuber';
+
+
+let mostProfitableNeighborhood = 'Chelsea'
+    console.log(mostProfitableNeighborhood);
+
+let companyCeo = 'Susan Smith'
+    console.log(companyCeo);
